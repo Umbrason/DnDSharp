741b98270c2948879c4448da0a517734a9243380
Umbrason <Umbrason@Outlook.com>
2025-07-07 07:12:05 +0200

    Create ClassLevelGenerator.cs

---
+++ Core/Classes/ClassLevelGenerator.cs
@@ -0,0 +1,57 @@
+using System.Diagnostics.CodeAnalysis;
+using DnDSharp.Core;
+using DnDSharp.Vanilla;
+
+
+
+/*
+
+Nice to have:
+just write a ctor in IClassLevel
+ctor gets parsed via reflections to create the ClassLevelGenerator
+can ctor params have attributes? HOLY SHIT YES THEY CAN!
+
+ok this is the plan:
+0. always needs a Character to reference to check for existing classes
+1. determine available subclasses if is abstract IClassLevel
+2. read option metadata from ctor parameter attributes (i.e. [Options(Wizard.Spelllist(1).Concat(Wizard.Spelllist(2)))]IActivity[] spells) 
+*/
+public class ClassLevelGenerator<T> where T : IClassLevel
+{
+    public ClassLevelGenerator([NotNull()]int value)
+    {
+        if (typeof(T).IsAbstract)
+        {
+
+        }
+    }
+
+    public ChoiceDescription CurrentChoice => requiredChoices[choiceDescriptionIndex];
+    private List<ChoiceDescription> requiredChoices;
+    private List<object[]> choices;
+
+
+    int choiceDescriptionIndex = 0;
+    public struct ChoiceDescription
+    {
+        public string ID;
+        public int minPicks;
+        public int maxPicks;
+        public object[] options;
+        public Type optionType;
+    }
+
+    public bool IsChoiceValid(int[] indices)
+    {
+        if (indices.Length > CurrentChoice.maxPicks) return false;
+        if (indices.Length < CurrentChoice.minPicks) return false;
+        if (indices.Distinct().Count() != indices.Length) return false;
+        if (indices.Any(i => i < 0 || i >= CurrentChoice.options.Length)) return false;
+        return true;
+    }
+
+    public IClassLevel Create()
+    {
+        return null;
+    }
+}
